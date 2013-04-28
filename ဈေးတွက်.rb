6e6118854551d821fc529533a50485bb858c3479
Thar Htet <kotharthar@gmail.com>
2013-04-28 15:26:34 +0630

    Code clean up and make beautiful label name for various methods. Symbol (in burmese number) to/fro ASCII number is added. The main purpose is to make the code as burmese as possible and beautiful. To be able to replace the Keyword such as require, if , else, def, end we will need to re-compile Ruby interpreter.

---
+++ ဈေးတွက်.rb
@@ -0,0 +1,19 @@
+class Hash
+	def တန်ဖိုးအားလုံးပေါင်း
+		self.values.inject(:+)
+	end
+end
+
+def ရေး(စာ) puts စာ; end
+
+def တန်ဖိုး(lbl)
+	n = %w(0 1 2 3 4 5 6 7 8 9 .)
+	un = %w(၀ ၁ ၂ ၃ ၄ ၅ ၆ ၇ ၈ ၉ .)
+	(lbl.to_s.split("").collect do |c| n[un.index(c)] unless un.index(c).nil?;end).join("").to_i
+end
+
+def စာ(val)
+	un = %w(0 1 2 3 4 5 6 7 8 9 .)
+	n = %w(၀ ၁ ၂ ၃ ၄ ၅ ၆ ၇ ၈ ၉ .)
+	(val.to_s.split("").collect do |c| n[un.index(c)] unless un.index(c).nil?;end).join("")
+end
